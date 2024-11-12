c1b79415075ffe5cf6de220cbac7ca76dbc9cb86
hrishd <hrishd@abv.bg>
2024-11-12 23:42:53 +0200

    CanCard

---
+++ src/components/CanCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="can-card">
+      <h2>{{ can.name }}</h2>
+      <img :src="can.image" alt="Can Image" />
+      <p>{{ can.description }}</p>
+      <button @click="deleteCan">Delete</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps({ can: Object });
+  const emit = defineEmits(['delete-can']);
+  const deleteCan = () => {
+    emit('delete-can', props.can.name);
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
