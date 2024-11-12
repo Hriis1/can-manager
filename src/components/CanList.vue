41d7511792df8f163fb7952738c03d51bc82469a
hrishd <hrishd@abv.bg>
2024-11-12 23:42:25 +0200

    base CanList component

---
+++ src/components/CanList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="can-list">
+      <CanCard v-for="can in cans" :key="can.name" :can="can" @delete-can="deleteCan" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from 'vue';
+  import CanCard from './CanCard.vue';
+  
+  const props = defineProps({ cans: Array });
+  const emit = defineEmits(['delete-can']);
+  const deleteCan = (canName) => {
+    emit('delete-can', canName);
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
