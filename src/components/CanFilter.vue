c7c9863109583f0f4f55cc0f154e18649daff29e
hrishd <hrishd@abv.bg>
2024-11-12 23:43:22 +0200

    CanFilter

---
+++ src/components/CanFilter.vue
@@ -0,0 +1,17 @@
+<template>
+    <input v-model="filterText" @input="filterChanged" placeholder="Filter Cans by Name" />
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  const filterText = ref('');
+  
+  const emit = defineEmits(['filter-change']);
+  const filterChanged = () => {
+    emit('filter-change', filterText.value);
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
